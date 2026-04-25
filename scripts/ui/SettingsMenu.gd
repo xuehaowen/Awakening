98ebec3bdcb5e9c22c74ccaa88cfb301f20680a9
xuehaowen <haowenxue.vin@gmail.com>
2026-04-25 13:18:45 -0700

    feat(settings): add config file persistence for audio volumes
    Implement ConfigFile save/load for settings:
    - Save Master/SFX/Music/Ambient volumes to user://settings.cfg
    - Load from config on startup, fallback to defaults if missing
    - Apply loaded values to audio buses immediately
    - Removes TODO comment - feature now complete

--- scripts/ui/SettingsMenu.gd
+++ scripts/ui/SettingsMenu.gd
@@ -99,22 +99,58 @@ func _db_to_slider(db: float) -> float:
 	return db_to_linear(db) * 100.0
 
 func _load_settings() -> void:
-	# Load from config or use defaults
-	master_slider.value = _db_to_slider(AudioServer.get_bus_volume_db(0))
+	"""Load settings from config file or use defaults"""
+	var config = ConfigFile.new()
+	var err = config.load("user://settings.cfg")
 	
-	var sfx_idx = AudioServer.get_bus_index("SFX")
-	if sfx_idx >= 0:
-		sfx_slider.value = _db_to_slider(AudioServer.get_bus_volume_db(sfx_idx))
-	
-	var music_idx = AudioServer.get_bus_index("Music")
-	if music_idx >= 0:
-		music_slider.value = _db_to_slider(AudioServer.get_bus_volume_db(music_idx))
-	
-	var ambient_idx = AudioServer.get_bus_index("Ambient")
-	if ambient_idx >= 0:
-		ambient_slider.value = _db_to_slider(AudioServer.get_bus_volume_db(ambient_idx))
+	if err == OK:
+		# Load from config file
+		master_slider.value = config.get_value("audio", "master_volume", 80.0)
+		sfx_slider.value = config.get_value("audio", "sfx_volume", 80.0)
+		music_slider.value = config.get_value("audio", "music_volume", 60.0)
+		ambient_slider.value = config.get_value("audio", "ambient_volume", 50.0)
+		
+		# Apply loaded values to audio buses
+		AudioServer.set_bus_volume_db(0, _slider_to_db(master_slider.value))
+		
+		var sfx_idx = AudioServer.get_bus_index("SFX")
+		if sfx_idx >= 0:
+			AudioServer.set_bus_volume_db(sfx_idx, _slider_to_db(sfx_slider.value))
+		
+		var music_idx = AudioServer.get_bus_index("Music")
+		if music_idx >= 0:
+			AudioServer.set_bus_volume_db(music_idx, _slider_to_db(music_slider.value))
+		
+		var ambient_idx = AudioServer.get_bus_index("Ambient")
+		if ambient_idx >= 0:
+			AudioServer.set_bus_volume_db(ambient_idx, _slider_to_db(ambient_slider.value))
+	else:
+		# Config doesn't exist, use current audio bus values as defaults
+		master_slider.value = _db_to_slider(AudioServer.get_bus_volume_db(0))
+		
+		var sfx_idx = AudioServer.get_bus_index("SFX")
+		if sfx_idx >= 0:
+			sfx_slider.value = _db_to_slider(AudioServer.get_bus_volume_db(sfx_idx))
+		
+		var music_idx = AudioServer.get_bus_index("Music")
+		if music_idx >= 0:
+			music_slider.value = _db_to_slider(AudioServer.get_bus_volume_db(music_idx))
+		
+		var ambient_idx = AudioServer.get_bus_index("Ambient")
+		if ambient_idx >= 0:
+			ambient_slider.value = _db_to_slider(AudioServer.get_bus_volume_db(ambient_idx))
 
 func _save_settings() -> void:
-	# TODO: Save to config file
-	# For now, settings persist only during session
-	pass
+	"""Save settings to config file"""
+	var config = ConfigFile.new()
+	
+	# Save current slider values
+	config.set_value("audio", "master_volume", master_slider.value)
+	config.set_value("audio", "sfx_volume", sfx_slider.value)
+	config.set_value("audio", "music_volume", music_slider.value)
+	config.set_value("audio", "ambient_volume", ambient_slider.value)
+	
+	# Save to user config
+	var err = config.save("user://settings.cfg")
+	if err != OK:
+		push_error("Failed to save settings: " + str(err))
